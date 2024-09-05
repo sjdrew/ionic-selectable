2113e0fcbd8afd0e0f6fff0e5fc755bbea9a6369
Doug Kneller <dkneller@gmail.com>
2024-09-04 21:20:47 -0600

    Commented out error and rebuilt

--- projects/ionic-selectable/src/lib/ionic-selectable.component.ts
+++ projects/ionic-selectable/src/lib/ionic-selectable.component.ts
@@ -1664,7 +1664,7 @@ export class IonicSelectableComponent implements ControlValueAccessor, OnInit, D
 
     return new Promise(function(resolve, reject) {
       if (!self._isEnabled || self._isOpened) {
-        reject('IonicSelectable is disabled or already opened.');
+        // reject('IonicSelectable is disabled or already opened.');
         return;
       }
 
